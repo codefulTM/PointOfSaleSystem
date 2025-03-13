ef2f8d7e3209219fd5a7a2d52be6d57d981bb019
04nhatminh <04nhatminh@gmail.com>
2025-03-13 19:54:59 +0700

    Update UI: Customer Details Window

--- PointOfSaleSystem/Views/CustomerDetailsWindow.xaml.cs
+++ PointOfSaleSystem/Views/CustomerDetailsWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +12,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,19 +19,13 @@ using Windows.Foundation.Collections;
 namespace PointOfSaleSystem.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class CustomerPage : Page
+    public sealed partial class CustomerDetailsWindow : Window
     {
-        public CustomerPage()
+        public CustomerDetailsWindow()
         {
             this.InitializeComponent();
-        }
-
-        private void viewDetailButton_Click(object sender, RoutedEventArgs e)
-        {
-            var screen = new CustomerDetailsWindow();
-            screen.Activate();
         }
     }
 }
