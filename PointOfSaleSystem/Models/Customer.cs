5dc284c859d3a8f6e9ac042f0139737fc4eacf2d
codefulTM <nguyentuanminh435@gmail.com>
2025-03-17 23:21:28 +0700

    Finished functions Update and Delete for CustomerRepository class.

---
+++ PointOfSaleSystem/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleSystem.Models
+{
+    public class Customer
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Gender { get; set; }
+    }
+}
