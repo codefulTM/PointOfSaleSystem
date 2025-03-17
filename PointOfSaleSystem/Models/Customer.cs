297a33692e92a634d16478c9c497c6738f7ffcef
04nhatminh <04nhatminh@gmail.com>
2025-03-17 17:22:12 +0700

    Initialize Customer.cs and CustomerRepository.cs

---
+++ PointOfSaleSystem/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Phone { get; set; } = null;
+        public string? Address { get; set; } = null;
+        public DateOnly? DateOfBirth { get; set; } = null;
+        public string? Gender { get; set; } = null;
+    }
+}
+
