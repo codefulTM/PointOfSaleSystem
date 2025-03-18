09f0066c712aa0ce2b390a437f2d235740120592
04nhatminh <04nhatminh@gmail.com>
2025-03-18 18:29:11 +0700

    Modified variables in Customer for merging

--- PointOfSaleSystem/Models/Customer.cs
+++ PointOfSaleSystem/Models/Customer.cs
@@ -8,12 +8,11 @@ namespace PointOfSaleSystem.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string? Phone { get; set; } = null;
-        public string? Address { get; set; } = null;
-        public DateOnly? DateOfBirth { get; set; } = null;
-        public string? Gender { get; set; } = null;
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Gender { get; set; }
     }
 }
-
--- PointOfSaleSystem/Models/CustomerRepository.cs
+++ PointOfSaleSystem/Models/CustomerRepository.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Npgsql;
+using Windows.ApplicationModel.Store;
 
 namespace PointOfSaleSystem.Models
 {
-    public class CustomerRepository : IRepository<Customer>
+    class CustomerRepository : IRepository<Customer>
     {
         List<Customer> customers = new List<Customer>();
         private NpgsqlConnection _connection;
@@ -29,15 +30,35 @@ namespace PointOfSaleSystem.Models
             }
             return _instance;
         }
+
+
+        public void Delete(int id)
+        {
+            string query = "UPDATE CUSTOMER SET deleted = TRUE WHERE customer_id = @id";
+            using (var cmd = new NpgsqlCommand(query, _connection))
+            {
+                cmd.Parameters.AddWithValue("id", id);
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+                _connection.Close();
+            }
+
+            var customerToRemove = customers.FirstOrDefault(c => c.Id == id);
+            if (customerToRemove is not null)
+            {
+                customers.Remove(customerToRemove);
+            }
+        }
+
         public void Create(Customer entity)
         {
             string query = "INSERT INTO CUSTOMER(name, phone_number, address, birthday, gender) VALUES(@name, @phone_number, @address, @birthday, @gender) RETURNING customer_id;";
             using (var cmd = new NpgsqlCommand(query, _connection))
             {
                 cmd.Parameters.AddWithValue("name", entity.Name);
-                cmd.Parameters.AddWithValue("phone_number", entity.Phone);
+                cmd.Parameters.AddWithValue("phone_number", entity.PhoneNumber);
                 cmd.Parameters.AddWithValue("address", entity.Address);
-                cmd.Parameters.AddWithValue("birthday", entity.DateOfBirth);
+                cmd.Parameters.AddWithValue("birthday", entity.Birthday);
                 cmd.Parameters.AddWithValue("gender", entity.Gender);
 
                 _connection.Open();
@@ -48,10 +69,6 @@ namespace PointOfSaleSystem.Models
             }
         }
 
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
 
         public IEnumerable<Customer> GetAll()
         {
@@ -69,9 +86,9 @@ namespace PointOfSaleSystem.Models
                             Customer customer = new Customer();
                             customer.Id = reader.GetInt32(0);
                             customer.Name = reader.GetString(1);
-                            customer.Phone = reader.GetString(2);
+                            customer.PhoneNumber = reader.GetString(2);
                             customer.Address = reader.GetString(3);
-                            customer.DateOfBirth = DateOnly.FromDateTime(reader.GetDateTime(4));
+                            customer.Birthday = reader.GetDateTime(4);
                             customer.Gender = reader.GetString(5);
                             customers.Add(customer);
                         }
@@ -98,7 +115,30 @@ namespace PointOfSaleSystem.Models
 
         public void Update(Customer entity)
         {
-            throw new NotImplementedException();
+            string query = "UPDATE CUSTOMER " +
+                "SET name = @name, phone_number = @phone_number," +
+                "address = @address, birthday = @birthday, gender = @gender" +
+                "WHERE customer_id = @id";
+
+            using (var cmd = new NpgsqlCommand(query, _connection))
+            {
+                cmd.Parameters.AddWithValue("name", entity.Name);
+                cmd.Parameters.AddWithValue("phone_number", entity.PhoneNumber ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("address", entity.Address ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("birthday", entity.Birthday ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("gender", entity.Gender ?? (object)DBNull.Value);
+
+                _connection.Open();
+                cmd.ExecuteNonQuery();
+                _connection.Close();
+            }
+
+            var existingProduct = customers.FirstOrDefault(c => c.Id == entity.Id);
+            if (existingProduct != null)
+            {
+                customers.Remove(existingProduct);
+                customers.Add(entity);
+            }
         }
     }
 }
