d2df5ebc125c05d76c01cc6c21946ad2af4718a0
codefulTM <nguyentuanminh435@gmail.com>
2025-04-05 11:26:45 +0700

    Add QR code functionality and new payment windows
    - Updated `PointOfSaleSystem.csproj` to include `QRCoder` and added `AddMomoPaymentMethodWindow.xaml`.
    - Modified `PointOfSaleSystem.csproj.user` to include new pages for `ShowMomoQRWindow.xaml`, `AddMomoPaymentMethodWindow.xaml`, and `ShowBankQRWindow.xaml`.
    - Enhanced `PaymentMethodPage.xaml` with a button for displaying bank QR codes and named the Momo payment methods `ListView`.
    - Made `Bank_SetDefaultClick` and `Momo_SetDefaultClick` methods asynchronous in `PaymentMethodPage.xaml.cs`, and added methods for showing bank and Momo QR codes.
    - Created `AddMomoPaymentMethodWindow.xaml` and its code-behind to handle adding Momo payment methods.
    - Introduced `ShowBankQRWindow.xaml` and its code-behind for displaying bank QR codes.
    - Added `ShowMomoQRWindow.xaml` and its code-behind, currently without implementation.

---
+++ PointOfSaleSystem/Views/ShowBankQRWindow.xaml.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using PointOfSaleSystem.Models;
+using Microsoft.UI.Xaml.Media.Imaging;
+using QRCoder;
+using Windows.Storage.Streams;
+using System.Security.Principal;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PointOfSaleSystem.Views
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShowBankQRWindow : Window
+    {
+        public PaymentMethod PaymentMethodInfo { get; set; }
+        public int Amount { get; set; } = 0;
+        public Dictionary<string, string> BankCodes = new Dictionary<string, string>
+        {
+            { "Vietcombank", "VCB" },
+            { "VietinBank", "CTG" },
+            { "BIDV", "BID" },
+            { "Techcombank", "TCB" },
+            { "MB Bank", "MBB" },
+            { "ACB", "ACB" },
+            { "VPBank", "VPB" },
+            { "HDBank", "HDB" },
+            { "Sacombank", "STB" },
+            { "SHB", "SHB" },
+            { "Eximbank", "EIB" },
+            { "TPBank", "TPB" },
+            { "SeABank", "SEAB" },
+            { "VIB", "VIB" },
+            { "OCB", "OCB" },
+            { "ABBANK", "ABB" },
+            { "SCB", "SCB" },
+            { "Bac A Bank", "BAB" },
+            { "PVcomBank", "PVC" },
+            { "Nam A Bank", "NAB" }
+        };
+
+
+        public ShowBankQRWindow(PaymentMethod paymentMethod, int amount)
+        {
+            this.PaymentMethodInfo = paymentMethod;
+            this.Amount = amount;
+            this.InitializeComponent();
+
+            // Generate the QR code
+            if(paymentMethod.Type != "bank")
+            {
+                return;
+            }
+            string qrUrl = $"https://img.vietqr.io/image/{BankCodes[paymentMethod.BankName]}-{paymentMethod.AccountNumber}-compact2.png?amount={amount}";
+            qrCodeImage.Source = new BitmapImage(new Uri(qrUrl));
+        }
+    }
+}
